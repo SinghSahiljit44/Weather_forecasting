841e51477f45a0b920db8bafdaaa31a0c7097f1c
SinghSahiljit44 <132672127+SinghSahiljit44@users.noreply.github.com>
2026-07-17 21:28:17 +0200

    won't use drive for dataset, tommorow i will find out another way

--- src/config.py
+++
@@ -1,7 +0,0 @@
-import os
-
-DRIVE_BASE_PATH = '/content/drive/MyDrive/mldm_project_data'
-RAW_DATA_PATH = os.path.join(DRIVE_BASE_PATH, 'raw')
-PROCESSED_DATA_PATH = os.path.join(DRIVE_BASE_PATH, 'processed')
-
-RANDOM_SEED = 42
